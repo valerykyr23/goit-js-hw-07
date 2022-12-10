cc59ecddce5129ebea58c021eb8af9de2887001f
Valeriia Kyrychenko <valeriia.kyrychenko22@gmail.com>
2022-12-11 00:36:02 +0200

    добавляет черновик решения задачи №1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,3 +3,36 @@ import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
 
+const list = document.querySelector(".gallery");
+
+console.log(list);
+
+// const markup = galleryItems.reduce((acc, { preview,description }) => acc + `<li><img src="${preview}"  alt="${description}" ></li>`, "");
+const markup = galleryItems.map(({original,preview,description} )=> `<a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>`).join("");
+
+list.insertAdjacentHTML("beforeend", markup);
+
+console.log(basicLightbox);
+
+list.addEventListener("click", handleClick);
+
+
+
+function handleClick(evt) {
+    event.preventDefault();
+    
+const instance = basicLightbox.create(`
+	 <img src="assets/images/image.png" width="800" height="600">
+`);
+
+    instance.show();
+
+}
+
