763ebc382892e7992707e60f0c8d47607e2382bb
Valeriia Kyrychenko <valeriia.kyrychenko22@gmail.com>
2022-12-10 22:37:15 +0200

    добавляет папку со стартовыми файлами для дз

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,6 +2,3 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
-
-const galleryList = document.querySelector(gallery);
-console.log(galleryList);
